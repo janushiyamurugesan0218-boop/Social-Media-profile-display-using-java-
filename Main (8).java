fa87ef2d00f9880c85f61b2673da9e323fad7226
Janushiya <janushiyamurugesan0218@gmail.com>
2026-07-03 22:00:39 +0530

    Add files via upload

---
+++ Main (8).java
@@ -0,0 +1,13 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+       String name="Janushiya";
+	   int Platform=25000;
+	   
+		System.out.println("Name="+name);
+		System.out.println(Platform+"/Platform");
+		System.out.println("Youtube");
+	}
+}
